5d38f75a43d39c1cc93a61b19d0d9d0390cdf7d2
SalvadorLekan <lekansalvador@gmail.com>
2020-09-10 11:41:21 -0400

    Added facebook Share button

--- js/index.js
+++ js/index.js
@@ -15,11 +15,13 @@ const weatherImage=document.getElementById('weather-image');
 const humidity=document.getElementById('humidity');
 const iframe=document.getElementById('iframe');
 const temp =document.getElementById('temp');
+const share =document.getElementById('share');
 
 form.addEventListener('submit',e=>{
     e.preventDefault();
     if(address.value&&locatio.value){
         const fullAddress=`${address.value.trim()},${locatio.value.trim()}`.replace(/ +/g,'+');
+        const facebookShare=fullAddress.replace(/\+/g,'%2B').replace(/,/g,'%2C')
         const state=locatio.value.trim().replace(/ +[, ]+|[, ]+/g,' ').split(' ')[0]
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=628904fcbcc368d6b240913148864136`)
         .then(data=>data.json())
@@ -34,7 +36,9 @@ form.addEventListener('submit',e=>{
             locatio.value=''
             address.value=''
         })
+        console.log(facebookShare)
         iframe.setAttribute('src',`https://www.google.com/maps/embed/v1/place?key=AIzaSyBby-Z83HvfSVAaAGGRwrV5O9AM83QBUcI&q=${fullAddress}`)
+        share.setAttribute('src',`https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fwww.google.com%2Fmaps%2Fsearch%2F%3Fapi%3D1%26query%3D${facebookShare}&layout=button&size=large&width=77&height=28&appId`)
     }
 })
 
